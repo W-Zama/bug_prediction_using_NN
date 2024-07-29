51fe7eb972dbec207f186d7da3f9b1ef859071ba
W-Zama <wzama4667@gmail.com>
2024-07-29 17:10:13 +0900

    add pytorch program

--- models/model.py
+++ models/model.py
@@ -1,6 +1,20 @@
 import torch
+import torch.nn as nn
 
-class model(torch.nn.Module):
-    def __init_(self):
-        super(model, self).__init__()
-        self.fc
+
+class PredictModel(torch.nn.Module):
+    def __init__(self):
+        super(PredictModel, self).__init__()
+        self.fc1 = nn.Linear(1, 512)
+        self.fc2 = nn.Linear(512, 512)
+        self.fc3 = nn.Linear(512, 512)
+        self.fc4 = nn.Linear(512, 1)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = torch.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
+
+model = PredictModel()
